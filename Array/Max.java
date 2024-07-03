e53cebe1a2103f1db11c4683146be70668a22f85
vinod <vinodstm74@gmail.com>
2024-07-03 20:07:31 +0530

    new

---
+++ Array/Max.java
@@ -0,0 +1,16 @@
+
+public class Max {
+
+    public static void main(String[] args) {
+
+        int arr[] = { 10, 50, 60, 40, 50 ,500};
+        int max = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
